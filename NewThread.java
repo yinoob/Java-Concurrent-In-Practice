900e6ab21eff480fa24c40f89534e59aecbbb9ec
wanyisong <wanyisong@9clouds.com.cn>
2021-11-19 01:27:40 +0800

    通过继承Thread和实现Runnable新建线程

---
+++ NewThread.java
@@ -0,0 +1,13 @@
+public class NewThread {
+    public static class NewThreadExtend extends Thread{
+        @Override
+        public void run(){
+            System.out.println("这是我的新线程，继承Thread类实现");
+        }
+    }
+
+    public static void main(String[] args){
+        NewThreadExtend newThreadExtend = new NewThreadExtend();
+        newThreadExtend.run();
+    }
+}
---
+++ NewThread1.java
@@ -0,0 +1,23 @@
+public class NewThread1 {
+    public static class NewThreadRunnable implements Runnable{
+        @Override
+        public void run(){
+            System.out.println("这是我的新线程，实现Runnable接口实现");
+        }
+    }
+
+    public static void main(String[] args){
+        NewThreadRunnable newThreadRunnable = new NewThreadRunnable();
+
+        new Thread(newThreadRunnable).start();
+        //new Thread(new NewThreadRunnable()).start();
+
+        /**错误示范
+         newThreadRunnable.run();
+         */
+
+        new Thread(() -> {
+            System.out.println("Java 8 匿名内部类");
+        }).start();
+    }
+}
