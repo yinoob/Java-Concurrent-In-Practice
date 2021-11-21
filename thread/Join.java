4ec1a9c1900e88a733513f5e8d1747bc7437e1f7
wanyisong <1243876958@qq.com>
2021-11-21 22:22:49 +0800

    yield方法实现

--- thread/Join.java
+++ thread/Join.java
@@ -1,10 +1,26 @@
 package thread;
 
 public class Join {
+    public static int i = 0;
     static class ThreadA implements Runnable{
         @Override
         public void run(){
             try{
+                i++;
+                System.out.println("sleep one second");
+                Thread.sleep(1000);
+                System.out.println("already sleep one second ");
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static class ThreadB implements Runnable{
+        @Override
+        public void run(){
+            try{
+                i++;
                 System.out.println("sleep one second");
                 Thread.sleep(1000);
                 System.out.println("already sleep one second ");
@@ -15,9 +31,14 @@ public class Join {
     }
 
     public static void main(String[] args)throws InterruptedException{
-        Thread thread = new Thread(new ThreadA());
-        thread.start();
-        //thread.join();
+        Thread threadA = new Thread(new ThreadA());
+        Thread threadB = new Thread(new ThreadA());
+        threadA.start();
+        threadB.start();
+        //threadA.join();
+        //threadB.join();
+        System.out.println("i的值为"+i);
         System.out.println("如果不加join方法，我会先被打出来，加了就不一样了");
+
     }
 }
---
+++ thread/YieldTest.java
@@ -0,0 +1,25 @@
+package thread;
+
+public class YieldTest implements Runnable{
+    YieldTest(){
+        Thread t = new Thread(this);
+        t.start();
+    }
+    public void run(){
+        for(int i = 0;i<5;i++){
+            if((i%5)==0){
+                System.out.println(Thread.currentThread()+"yield cpu");
+                Thread.yield();
+            }
+        }
+        System.out.println(Thread.currentThread()+"is over");
+    }
+
+    public static void main(String[] args){
+
+        new YieldTest();
+        new YieldTest();
+        new YieldTest();
+
+    }
+}
