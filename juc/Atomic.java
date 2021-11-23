e2611b66f08fadc9dfe0ba0f186501b1198dc17a
wanyisong <1243876958@qq.com>
2021-11-23 23:41:04 +0800

    AtomicLong实现

---
+++ juc/Atomic.java
@@ -0,0 +1,45 @@
+package juc;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Atomic {
+    private static AtomicLong atomicLong = new AtomicLong();
+
+    private static Integer[] arrayOne = new Integer[]{0,1,2,3,0,5,6,0,54,0};
+    private static Integer[] arrayTwo = new Integer[]{10,1,2,3,0,5,6,0,54,7};
+
+    public static void main(String[] args)throws InterruptedException{
+
+        Thread threadOne = new Thread(new Runnable(){
+            @Override
+            public void run(){
+                int size = arrayOne.length;
+                for(int i=0;i<size;++i){
+                    if(arrayOne[i].intValue() == 0){
+                        atomicLong.incrementAndGet();
+                    }
+                }
+            }
+        });
+
+        Thread threadTwo = new Thread(new Runnable(){
+            @Override
+            public void run(){
+                int size = arrayTwo.length;
+                for(int i=0;i<size;++i){
+                    if(arrayTwo[i].intValue() == 0){
+                        atomicLong.incrementAndGet();
+                    }
+                }
+            }
+        });
+
+        threadOne.start();
+        threadTwo.start();
+
+        threadOne.join();
+        threadTwo.join();
+
+        System.out.println("count:" + atomicLong.get());
+    }
+}
