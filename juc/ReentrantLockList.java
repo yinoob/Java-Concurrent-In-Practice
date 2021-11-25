cb29c6c626ddfb69737abea8841a9dba1c3035cb
wanyisong <1243876958@qq.com>
2021-11-25 13:41:07 +0800

    独占锁和读写锁实现线程安全的list

---
+++ juc/ReentrantLockList.java
@@ -0,0 +1,37 @@
+package juc;
+
+import java.util.ArrayList;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockList {
+
+    private ArrayList<String> array = new ArrayList<String>();
+
+    private volatile ReentrantLock lock = new ReentrantLock();
+
+    public void add(String e){
+        lock.lock();
+        try{
+            array.add(e);
+        }finally {
+            lock.unlock();
+        }
+    }
+    public void remove(String e){
+        lock.lock();
+        try{
+            array.remove(e);
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    public String get(int index){
+        lock.lock();
+        try{
+            return array.get(index);
+        }finally {
+            lock.unlock();
+        }
+    }
+}
---
+++ juc/ReentrantReadWriteLockTest.java
@@ -0,0 +1,40 @@
+package juc;
+
+import java.util.ArrayList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public static class ReentrantReadWriteLockTest {
+    private ArrayList<String> array = new ArrayList<String>();
+
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    private final Lock readLock = lock.readLock();
+    private final Lock writeLock = lock.writeLock();
+
+
+    public void add(String e){
+        writeLock.lock();
+        try{
+            array.add(e);
+        }finally {
+            writeLock.unlock();
+        }
+    }
+    public void remove(String e){
+        writeLock.lock();
+        try{
+            array.remove(e);
+        }finally {
+            writeLock.unlock();
+        }
+    }
+
+    public String get(int index){
+        readLock.lock();
+        try{
+            return array.get(index);
+        }finally {
+            readLock.unlock();
+        }
+    }
+}
